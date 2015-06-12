583e1699de42e20d40cf3e053b387fc728042e4e
James Kamau <jameskmw48@gmail.com>
2015-06-12 10:31:19 +0100

    mial fix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -52,50 +52,6 @@ Route::get('test-mailer', function() {
     return $mail->send() ? 'Success' : 'Failure';
 });
 
-Route::get('test', function() {
-   
-// multiple recipients
-    $to = 'jameskmw48@gmail.com';
-
-// subject
-    $subject = 'Birthday Reminders for August';
-
-// message
-    $message = '
-<html>
-<head>
-  <title>Birthday Reminders for August</title>
-</head>
-<body>
-  <p>Here are the birthdays upcoming in August!</p>
-  <table>
-    <tr>
-      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
-    </tr>
-    <tr>
-      <td>Image</td><td>3rd</td><td><img src="http://yourdomain.com/images/example.jpg"></td><td>1970</td>
-    </tr>
-    <tr>
-      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
-    </tr>
-  </table>
-</body>
-</html>
-';
-
-// To send HTML mail, the Content-type header must be set
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-// Additional headers
-    //$headers .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
-    $headers .= 'From: Birthday Reminder <birthday@example.com>' . "\r\n";
-
-
-// Mail it
-   return mail($to, $subject, $message, $headers)? 'Success' :' Failure';
-});
-
 
 
 
