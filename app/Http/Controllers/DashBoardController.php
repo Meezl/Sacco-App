ad176ef598022f84ba76d22a4e6e28e6d5fd673d
James kamau <jameskmw48@gmail.com>
2015-06-23 10:07:22 -0700

    26 class A meeting

--- app/Http/Controllers/DashBoardController.php
+++ app/Http/Controllers/DashBoardController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use App\Models\Message;
@@ -8,15 +9,19 @@ use App\Models\Message;
  *
  * @author James
  */
-class DashBoardController  extends Controller{
-    
+class DashBoardController extends Controller {
+
     public function getIndex() {
         $stats = \DB::table('messages')
                 ->select(\DB::raw('count(*) as count, ucase(text) as text'))
-                ->where('text', '=', 'yes')
-                ->orWhere('text', '=', 'no')
+                ->where('created_at', '>', '2015-06-20 00:00:00')
+                ->where(function($query) {
+                    $query->where('text', '=', 'yes')
+                    ->orWhere('text', '=', 'no');
+                })
                 ->groupBy('text')
                 ->get();
         return view('dashboard', compact('stats'));
-    }    
+    }
+
 }
